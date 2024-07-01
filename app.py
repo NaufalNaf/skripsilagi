eff8679ae160a5a5efa08b17b7f71624934094ea
NaufalNaf <97237976+NaufalNaf@users.noreply.github.com>
2024-07-01 19:01:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,17 @@ st.title('Klasifikasi Tingkat Obesitas')
 height = st.number_input('Tinggi (m)', min_value=0.5, max_value=2.5, value=1.75)
 weight = st.number_input('Berat Badan (kg)', min_value=20, max_value=200, value=70)
 gender = st.selectbox('Jenis kelamin', ('Female', 'Male'))
+st.write("Inputs received.")
 
 # Konversi gender ke bentuk numerik
 gender_num = 0 if gender == 'Female' else 1
 
 # Prediksi
-input_data = np.array([[height, weight, gender_num]])
-prediction = clf.predict(input_data)
+if st.button('Predict'):
+    st.write("Button clicked.")
+    input_data = np.array([[height, weight, gender_num]])
+    prediction = model.predict(input_data)
+    st.write("Prediction made.")
 
 # Tampilkan hasil
 obesity_level = ['Normal', 'Overweight', 'Obese']
