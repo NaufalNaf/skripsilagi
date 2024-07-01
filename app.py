cebe3d0a350bc5ed8ec5ddc9ee5678b74517c158
NaufalNaf <97237976+NaufalNaf@users.noreply.github.com>
2024-07-01 15:57:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ gender_num = 0 if gender == 'Female' else 1
 
 # Prediksi
 input_data = np.array([[height, weight, gender_num]])
-prediction = model.predict(input_data)
+prediction = models.predict(input_data)
 
 # Tampilkan hasil
 obesity_level = ['Normal', 'Overweight', 'Obese']
