913c8c19637948f10ddeba3293fed9750d53c654
NaufalNaf <97237976+NaufalNaf@users.noreply.github.com>
2024-07-01 15:41:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ clf = joblib.load('models/scaler.sav')
 st.title('Klasifikasi Tingkat Obesitas')
 
 # Input pengguna
-height = st.number_input('Tinggi (cm)', min_value=0.5, max_value=2.5, value=1.75)
+height = st.number_input('Tinggi (m)', min_value=0.5, max_value=2.5, value=1.75)
 weight = st.number_input('Berat Badan (kg)', min_value=20, max_value=200, value=70)
 age = st.number_input('Umur)', min_value=1, max_value=120, value=25)
 gender = st.selectbox('Jenis kelamin', ('Female', 'Male'))
